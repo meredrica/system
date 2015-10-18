389b03dcf3eddefc842a56ce0fa5288dfce6f96c
meredrica <stuff@meredrica.org>
2015-10-18 03:46:08 +0200

    updates sync.sh

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@ cp -v /home/meredrica/.zshrc ./h/
 cp -v /home/meredrica/.vimrc ./h/
 cp -v /home/meredrica/.config/awesome/rc.lua ./h/.config/awesome/rc.lua
 cp -v /home/meredrica/.config/lilyterm/default.conf ./h/.config/lilyterm/default.conf
+cp -v /home/meredrica/.oh-my-zsh/themes/meredrica.zsh-theme ./h/.oh-my-zsh/themes/meredrica.zsh-theme
 
 git add .
 git commit
